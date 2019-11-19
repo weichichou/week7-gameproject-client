ae603794e85b5dd2a34ef4e72bbeda2bf12fd14a
weichichou <weichi.cc@gmail.com>
2019-11-19 12:10:51 +0100

    Join button connet to database

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import superagent from 'superagent'
+import {connect} from 'react-redux'
 
-export default class DetailPage extends React.Component{
+class DetailPage extends React.Component{
+
+    handleClick = () => {
+        console.log('Got Clicked?')
+        console.log('this.props.user.id', this.props.user.id)
+        superagent
+            .put(`http://localhost:4000/join/${this.props.match.params.id}`)
+            .send({userId: this.props.user.id})
+            .then(res => console.log(res.body))
+    }
+    
     render(){
         return(
-            <button>Join</button>
+            <button onClick={this.handleClick}>Join</button>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return { user: state.user };
+  };
+  
+  export default connect(mapStateToProps)(DetailPage);
